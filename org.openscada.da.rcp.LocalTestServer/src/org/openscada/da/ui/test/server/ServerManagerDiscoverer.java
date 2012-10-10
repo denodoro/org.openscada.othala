cf6d4e0c1124dad9aee1f7398b4252868e989de5
Jens Reimann <jens.reimann@th4-systems.com>
2012-10-10 15:55:51 +0200

    cleanup

--- org.openscada.da.rcp.LocalTestServer/src/org/openscada/da/ui/test/server/ServerManagerDiscoverer.java
+++ org.openscada.da.rcp.LocalTestServer/src/org/openscada/da/ui/test/server/ServerManagerDiscoverer.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.da.ui.test.server;
 
 import java.util.HashMap;
@@ -8,13 +27,12 @@ import java.util.Set;
 import org.openscada.core.ConnectionInformation;
 import org.openscada.core.ui.connection.AbstractConnectionDiscoverer;
 import org.openscada.core.ui.connection.ConnectionDescriptor;
-import org.openscada.core.ui.connection.ConnectionDiscoverer;
 import org.openscada.da.rcp.LocalTestServer.Activator;
 import org.openscada.da.rcp.LocalTestServer.ServerManager;
 import org.openscada.da.rcp.LocalTestServer.ServerManager.Entry;
 import org.openscada.da.rcp.LocalTestServer.ServerManager.ServerListener;
 
-public class ServerManagerDiscoverer extends AbstractConnectionDiscoverer implements ConnectionDiscoverer, ServerListener
+public class ServerManagerDiscoverer extends AbstractConnectionDiscoverer implements ServerListener
 {
 
     private final ServerManager serverManager;
